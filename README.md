db5f6d1341e56650940f4961e57ff0e8e54fcb63
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2025-11-30 21:25:01 -0800

    add docs
    Signed-off-by: Pedro Tôrres <t0rr3sp3dr0@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # truenas-clevis
 Clevis on TrueNAS Community
+
+## Getting Started
+
+1. Boot the TrueNAS installation disk in UEFI mode.
+
+2. On the _Console Setup_ screen, select _Shell_.
+
+3. Run the following command:
+
+```sh
+. <(curl -Ls https://t0rr3sp3dr0.github.io/truenas-clevis/make.sh)
+```
+
+4. Proceed the installation as usual. Your boot pool will be encrypted with the `nimda_saneurt` passphrase.
--- docs/make.sh
+++ docs/make.sh
@@ -8,7 +8,7 @@ sed -Ei 's|(\["zfs", "create",.+f"\{BOOT_POOL\}/ROOT"\])|["zfs", "set", "keyloca
 
 F='/usr/local/sbin/chroot'
 echo '#!/bin/bash -e' > "${F}"
-echo '[[ "${2}" = "grub-install" ]] && mkdir -p "${1}/boot/efi/EFI/debian" && curl -Lo "${1}/boot/efi/EFI/debian/grubx64.efi" "https://github.com/t0rr3sp3dr0/truenas-clevis/releases/latest/download/BOOTx64.EFI" && exec /usr/sbin/chroot "${1}" grub-install --version || exec /usr/sbin/chroot "${@}"' >> "${F}"
+echo '[[ "${2}" = "grub-install" ]] && mkdir -p "${1}/boot/efi/EFI/debian" && curl -Lo "${1}/boot/efi/EFI/debian/grubx64.efi" "https://github.com/t0rr3sp3dr0/truenas-clevis/releases/'"${RELEASE:-latest}"'/download/BOOTx64.EFI" && exec /usr/sbin/chroot "${1}" grub-install --version || exec /usr/sbin/chroot "${@}"' >> "${F}"
 chmod +x "${F}"
 
 F='/run/boot-pool.key'
